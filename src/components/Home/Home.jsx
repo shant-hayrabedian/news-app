66df9f8a94a2ebd7604737dd6f870bfc28b19919
shant <shanto.hayrabedian@gmail.com>
2021-05-28 16:31:19 +0400

    Home page sources card

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,26 @@
-
-
+import {Card} from 'antd';
+import './Home.css';
 const Home = () => {
-
+    const {Meta} = Card;
     return (
         <div>
             <h2>Sources</h2>
+            {/*    */}
+            <Card
+                style={{width: 300, background: '#C4C4C4'}}
+                actions={[
+                    <a>political</a>,
+                    <a>Language</a>,
+                    <a>country</a>
+                ]}
+            >
+                <Meta
+                    title="New York Times"
+                    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                      when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+                />
+            </Card>
         </div>
     )
 }
