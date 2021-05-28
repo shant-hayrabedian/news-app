92f0ce281e2dd6e734dd91936fe40807a870cc4f
Mayllo <haykohayrapetyan2@gmail.com>
2021-05-29 02:00:03 +0400

    synced PageNotFound component with master branch

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,12 +1,22 @@
-
+import { Result, Button } from 'antd';
+import {Link} from "react-router-dom";
 
 const NotFound = () => {
     return (
         <div className='not-found'>
             <h2>404 Error</h2>
             <p>Sorry Page Not Found</p>
+            {/*<h2>404 Error</h2>*/}
+            {/*<p>Sorry Page Not Found</p>*/}
+
+
+            <Result
+                status="404"
+                title="404"
+                subTitle="Sorry, the page you visited does not exist."
+                extra={<Button type="primary"><Link to="/">Back Home</Link></Button>}
+            />
         </div>
     )
 }
-
 export default NotFound;
